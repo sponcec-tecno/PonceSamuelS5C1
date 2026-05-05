8e772af81fcb2801c658674d859351f57cf5c7b5
sponcec-tecno <sponcec@unal.edu.co>
2026-05-05 11:16:22 -0500

    Agrego rk4 sin compararlo con el analítico

--- sala/PonceSamuel_S6C1_EDO.cpp
+++ sala/PonceSamuel_S6C1_EDO.cpp
@@ -12,6 +12,7 @@ double tf = 2.0; //tiempo final
 
 double f(double x);
 void euler(double h, int n, std::array<double, N> & sol, double y_0);
+void rk4(double h, int n, std::array<double, N> & sol, double y_0);
 void doc(std::array<double, N> & data, std::string name);
 
 int main(){
@@ -21,13 +22,23 @@ int main(){
 
 	//elements for euler
 	double h = (tf-ti)/N;//h for euler
-	std::array<double, N> sol;//where I'll save all the points for the solution
+	std::array<double, N> sol_e;//where I'll save all the points for the solution
 
 	//calling euler
-	euler(h, N, sol, y_0);
+	euler(h, N, sol_e, y_0);
+
+	//elements for rk4(Runge-Kutta 4)
+	std::array<double, N> sol_rk4;
+
+	//calling rk4
+	rk4(h, N, sol_rk4, y_0);
+
+	std::cout << sol_rk4[78] << "\n";
 
 	//making my data
-	doc(sol, "myout.dat");
+	doc(sol_e, "myout_e.dat");
+	doc(sol_rk4, "myout_rk.dat");
+
 	return 0;
 }
 
@@ -42,6 +53,25 @@ void euler(double h, int n, std::array<double, N> & sol, double y_0){
 	}
 }
 
+void rk4(double h, int n, std::array<double, N> & sol, double y_0){
+	sol[0] = y_0;
+	double k1 = 0.0;
+	double k2 = 0.0;
+	double k3 = 0.0;
+	double k4 = 0.0;
+	double aux = 0.0;
+
+        for (int i = 1; i<n; ++i){
+		k1 = f(sol[i-1]);
+		k2 = f(sol[i-1]+(k1/2));
+		k3 = f(sol[i-1]+(k2/2));
+		k4 = f(sol[i-1]+k3);
+		aux = k1 + (2*(k2+k3)) + k4;
+		sol[i] = sol[i-1]+(h*(1.0/6.0)*aux);
+        }
+}
+
+
 void doc(std::array<double, N> & data, std::string name){
     // Abro el archivo en modo escritura
     std::ofstream outfile;
@@ -52,8 +82,3 @@ void doc(std::array<double, N> & data, std::string name){
     //Cierro el archivo
     outfile.close();
 }
-
-
-//Crear un array que guarde los valores que la función va calculando
-//Crear la ecuación que determina mi punto siguiente
-//Llenar el array hasta que se cumpla cierto tiempo o número de pasos
