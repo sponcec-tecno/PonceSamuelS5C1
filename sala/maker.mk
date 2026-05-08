8314dd8dd1dbf6a9b50f3dfdbbfa811733aa119b
sponcec-tecno <sponcec@unal.edu.co>
2026-05-07 20:40:28 -0500

    Agregué para una la comparación de métodos y para el plot.py

--- sala/maker.mk
+++ sala/maker.mk
@@ -2,10 +2,9 @@ fig.png: myout_e.dat myout_rk.dat plot.py
 	python plot.py
 
 myout_e.dat: PonceSamuel_S6C1_EDO.cpp
+	g++ PonceSamuel_S6C1_EDO.cpp
+	./a.out
 
-
-myout_rk.dat: 
-
-PonceSamuel_S6C1_EDO.cpp:
+myout_rk.dat: PonceSamuel_S6C1_EDO.cpp
 	g++ PonceSamuel_S6C1_EDO.cpp
 	./a.out
