d08045ddf43ab4f2e0a7500c76566a93d701d950
sponcec-tecno <sponcec@unal.edu.co>
2026-05-08 17:14:57 -0500

    Maker más simple y sencillo, pude eliminar redundancia de instruccionas para .dat y .pdf

--- sala/maker.mk
+++ sala/maker.mk
@@ -1,16 +1,16 @@
-all.pdf: PLOTS_PonceSamuel_S6CASA_EDO.py myout_rk.dat myout_e.dat
-	python $<
-
-rk4.pdf: PLOTS_PonceSamuel_S6CASA_EDO.py myout_rk.dat
-	python $<
-
-euler.pdf: PLOTS_PonceSamuel_S6CASA_EDO.py myout_e.dat
-	python $<
-
-myout_rk.dat: PonceSamuel_S6C1_EDO.cpp
+.PHONY : data
+data : myout_euler.dat myout_rk4.dat
+%.dat : PonceSamuel_S6C1_EDO.cpp
 	g++ $<
 	./a.out
 
-myout_e.dat: PonceSamuel_S6C1_EDO.cpp
-	g++ $<
-	./a.out
+.PHONY : plots
+plots : all.pdf euler.pdf rk4.pdf
+
+%.pdf: PLOTS_PonceSamuel_S6CASA_EDO.py myout_euler.dat myout_rk4.dat
+	python $<
+
+.PHONY : clean
+clean :
+	rm -f *.dat
+	rm -f *.pdf
