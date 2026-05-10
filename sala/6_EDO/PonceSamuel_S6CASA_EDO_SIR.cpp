154fa4359eb651d87250181f162c12f34be03e24
sponcec-tecno <sponcec@unal.edu.co>
2026-05-10 12:17:29 -0500

    Se implementó rk4, la lógica entre códigos está buena, pero sigue sin solucionar el sistema

--- sala/6_EDO/PonceSamuel_S6CASA_EDO_SIR.cpp
+++ sala/6_EDO/PonceSamuel_S6CASA_EDO_SIR.cpp
@@ -6,14 +6,15 @@
 #include <string>
 #include <map>
 
-using funptr = std::function<double(double)>;
+using map = std::map<std::string, double>;
+using funptr = std::function<double(double, map&, map&)>;
 
 const int N = 200;
 
-double f_S(double people, std::map<std::string, double> & s, std::map<std::string, double> & p);
-double f_I(double people, std::map<std::string, double> & s, std::map<std::string, double> & p);
-double f_R(double people, std::map<std::string, double> & s, std::map<std::string, double> & p);
-double rk4(double h, double people, std::map<std::string, double> & p, std::map<std::string, double>  s, funptr f);
+double f_S(double people, map & s, map & p);
+double f_I(double people, map & s, map & p);
+double f_R(double people, map & s, map & p);
+double rk4(double h, double people, map & p, map s, funptr f, double y_0);
 void doc(std::array<double, N> & data, std::string name);
 
 int main(){
@@ -26,7 +27,7 @@ int main(){
 	double R0 = 0.0;
 	double people = S0+I0+R0;
 
-	std::map<std::string, double> params;//the parameters/constants for modify the system
+	map params;//the parameters/constants for modify the system
 	params["B"] = 3.0;
 	params["r"] = 1.0;
 
@@ -34,72 +35,91 @@ int main(){
 	//elements for euler
 	double N_double = static_cast<double>(N);
 	double h = (tf-ti)/(N_double-1.0);//h for euler
-	std::array<double, N> sol_S_euler;//saving the points for Susceptible
-	std::array<double, N> sol_I_euler;//saving the points for Infected
-	std::array<double, N> sol_R_euler;//saving the point for Recovered
+	std::array<double, N> S_euler;//saving the points for Susceptible
+	std::array<double, N> I_euler;//saving the points for Infected
+	std::array<double, N> R_euler;//saving the point for Recovered
 
 	//solving the system with euler
-	sol_S_euler[0] = S0;
-	sol_I_euler[0] = I0;
-	sol_R_euler[0] = R0;
+	S_euler[0] = S0;
+	I_euler[0] = I0;
+	R_euler[0] = R0;
+	map state_e;
 
         for (int i = 1; i<N; ++i){
-		sol_S_euler[i] = sol_S_euler[i-1]+(h*f_S(people, sol_I_euler[i-1], sol_S_euler[i-1], params));
-		sol_I_euler[i] = sol_I_euler[i-1]+(h*f_I(people, sol_I_euler[i-1], sol_S_euler[i-1], params));
-		sol_R_euler[i] = sol_R_euler[i-1]+(h*f_R(people, sol_I_euler[i-1], params));
+		state_e["S"] = S_euler[i-1];
+                state_e["I"] = I_euler[i-1];
+                state_e["R"] = R_euler[i-1];
+		S_euler[i] = S_euler[i-1]+(h*f_S(people, state_e, params));
+		I_euler[i] = I_euler[i-1]+(h*f_I(people, state_e, params));
+		R_euler[i] = R_euler[i-1]+(h*f_R(people, state_e, params));
 	}
 
 	//elements for rk4(Runge-Kutta 4)
 	std::array<double, N> S_rk4;//saving the points for Susceptible
         std::array<double, N> I_rk4;//saving the points for Infected
         std::array<double, N> R_rk4;//saving the point for Recovered
+	S_rk4[0] = S0;
+	I_rk4[0] = I0;
+	R_rk4[0] = R0;
 
 	//calling rk4
-	std::map<std::string, double> state;
+	map state_rk;
         for (int i = 1; i<N; ++i){
-		p["S"] = S[i-1];
-		p["I"] = I[i-1];
-		p["R"] = R[i-1];
-                S_rk4[i] = rk4(h, people, S_rk4[i-1], I_rk4[i-1], params, state, f_S);
-                I_rk4[i] = rk4(h, people, S_rk4[i-1], I_rk4[i-1], params, state, f_I);
-                R_rk4[i] = rk4(h, people, S_rk4[i-1], I_rk4[i-1], params, state, f_R);
+		state_rk["S"] = S_rk4[i-1];
+		state_rk["I"] = I_rk4[i-1];
+		state_rk["R"] = R_rk4[i-1];
+                S_rk4[i] = rk4(h, people, state_rk, params, f_S, S_rk4[i-1]);
+                I_rk4[i] = rk4(h, people, state_rk, params, f_I, I_rk4[i-1]);
+                R_rk4[i] = rk4(h, people, state_rk, params, f_R, R_rk4[i-1]);
         }
 
+
 	//making my data
-	doc(sol_S_euler, "myout_S_SIR.dat");
-	doc(sol_I_euler, "myout_I_SIR.dat");
-	doc(sol_R_euler, "myout_R_SIR.dat");
+	doc(S_euler, "myout_Se_SIR.dat");
+	doc(I_euler, "myout_Ie_SIR.dat");
+	doc(R_euler, "myout_Re_SIR.dat");
+        doc(S_rk4, "myout_Srk4_SIR.dat");
+        doc(I_rk4, "myout_Irk4_SIR.dat");
+        doc(R_rk4, "myout_Rrk4_SIR.dat");
 
 	return 0;
 }
 
-double f_S(double people, std::map<std::string, double> & s, std::map<std::string, double> & p){
-	return (-p["B"]*S*I)/people;
+double f_S(double people, map & s, map & p){
+	return (-p["B"]*s["S"]*s["I"])/people;
 }
 
-double f_I(double people, std::map<std::string, double> & s, std::map<std::string, double> & p){
-       return ((p["B"]*S*I)/people)-(p["r"]*I);
+double f_I(double people, map & s, map & p){
+       return ((p["B"]*s["S"]*s["I"])/people)-(p["r"]*s["I"]);
 }
 
-double f_R(double people, std::map<std::string, double> & s, std::map<std::string, double> & p){
-	return p["r"]*I;
+double f_R(double people, map & s, map & p){
+	return p["r"]*s["I"];
 }
 
-double rk4(double h, double people, std::map<std::string, double> & p, std::map<std::string, double> s, funptr f){
+double rk4(double h, double people, map & p, map s, funptr f, double y_0){
 
 	double k1 {};
 	double k2 {};
 	double k3 {};
 	double k4 {};
 	double aux {};
+	double S = s["S"];
+	double I = s["I"];
 
 	k1 = h*f(people, s, p);
-	s[""]
-	k2 = h*f(people, sol[i-1]+(k1/2.0));
-	k3 = h*f(sol[i-1]+(k2/2.0));
-	k4 = h*f(sol[i-1]+k3);
+	s["S"] = S+(k1/2.0);
+	s["I"] = I+(k1/2.0);
+	std::cout << S;
+	k2 = h*f(people, s, p);
+	s["S"] = S+(k2/2.0);
+	s["I"] = I+(k2/2.0);
+	k3 = h*f(people, s, p);
+        s["S"] = S+k3;
+        s["I"] = I+k3;
+	k4 = h*f(people, s, p);
 	aux = k1 + 2.0*k2 +2.0*k3 + k4;
-	sol[i] = sol[i-1]+((1.0/6.0)*aux);
+	return y_0+((1.0/6.0)*aux);
 }
 
 
--- sala/6_EDO/maker.mk
+++ sala/6_EDO/maker.mk
@@ -1,3 +1,4 @@
+#Ejercicio 1
 .PHONY : data1
 data1 : myout_euler_EDO.dat myout_rk4_EDO.dat
 %_EDO.dat : PonceSamuel_S6CASA_EDO.cpp
@@ -10,16 +11,24 @@ plot1 : all_EDO.pdf euler_EDO.pdf rk4_EDO.pdf
 %_EDO.pdf: PLOTS_PonceSamuel_S6CASA_EDO.py myout_euler_EDO.dat myout_rk4_EDO.dat
 	python $<
 
-.PHONY : data2
-data2 : myout_S_SIR.dat myout_I_SIR.dat myout_R_SIR.dat
+#Ejercicio SIR
+.PHONY : data2_e
+data2_e : myout_Se_SIR.dat myout_Ie_SIR.dat myout_Re_SIR.dat
+
+.PHONY : data2_rk4
+data2_rk4 : myout_Srk4_SIR.dat myout_Irk4_SIR.dat myout_Rrk4_SIR.dat
+
 %_SIR.dat : PonceSamuel_S6CASA_EDO_SIR.cpp
 	g++ $< -o a2.out
 	./a2.out
 
-.PHONY : plot2
-plot2 : euler_SIR.pdf
+.PHONY : plot2_e
+plot2_e : e_SIR.pdf e_SIR.pdf
 
-%_SIR.pdf: PLOTS_PonceSamuel_S6CASA_EDO_SIR.py myout_S_SIR.dat myout_I_SIR.dat myout_R_SIR.dat
+.PHONY : plot2_rk4
+plot2_rk4 : rk4_SIR.pdf rk4_SIR.pdf
+
+%_SIR.pdf: PLOTS_PonceSamuel_S6CASA_EDO_SIR.py myout_S%_SIR.dat myout_I%_SIR.dat myout_R%_SIR.dat
 	python $<
 
 .PHONY : clean
