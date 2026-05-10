3d9dcf43dd8067f2618fef94ba6482663541fa16
sponcec-tecno <sponcec@unal.edu.co>
2026-05-10 14:12:03 -0500

    Se regresaron los archivos a como estaban hechos bien sin rk4

--- sala/6_EDO/PLOTS_PonceSamuel_S6CASA_EDO_SIR.py
+++ sala/6_EDO/PLOTS_PonceSamuel_S6CASA_EDO_SIR.py
@@ -3,58 +3,30 @@ import matplotlib.pylab as plt
 
 
 #Creo mi array con el tiempo
+S=np.genfromtxt("myout_S_SIR.dat")
+I=np.genfromtxt("myout_I_SIR.dat")
+R=np.genfromtxt("myout_R_SIR.dat")
+N = S + I + R
+
 x = np.linspace(0.0, 60.0, 200)
 
-#Datos
-#euler
-S_e=np.genfromtxt("myout_Se_SIR.dat")
-I_e=np.genfromtxt("myout_Ie_SIR.dat")
-R_e=np.genfromtxt("myout_Re_SIR.dat")
-N_e = S_e + I_e + R_e
-
-#rk4
-S_rk4=np.genfromtxt("myout_Srk4_SIR.dat")
-I_rk4=np.genfromtxt("myout_Irk4_SIR.dat")
-R_rk4=np.genfromtxt("myout_Rrk4_SIR.dat")
-N_rk4 = S_rk4 + I_rk4 + R_rk4
 
 #Plots
-#euler
 fig, ((ax1, ax2)) = plt.subplots(2, 1, layout="constrained")
 
-ax1.plot(x, S_e, label="Susceptible", color="#F4F00B")
-ax1.plot(x, I_e, label="Infected", color="#C82909")
-ax1.plot(x, R_e, label="Recovered", color="#0BF41B")
+ax1.plot(x, S, label="Susceptible", color="#F4F00B")
+ax1.plot(x, I, label="Infected", color="#C82909")
+ax1.plot(x, R, label="Recovered", color="#0BF41B")
 ax1.set_xlabel('Tiempo(s)')
 ax1.set_ylabel('Número de personas')
 ax1.legend()
 ax1.set_title("Evolución de la población")
 
 ax2.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
-ax2.plot(x, N_e, color="#27E4F5")
+ax2.plot(x, N, color="#27E4F5")
 ax2.set_xlabel('Tiempo(s)')
 ax2.set_ylabel('Número de personas')
 ax2.set_title("Población en el sistema")
 
-plt.savefig("e_SIR.pdf")
-plt.close()
-
-#rk4
-fig, ((ax1, ax2)) = plt.subplots(2, 1, layout="constrained")
-
-ax1.plot(x, S_rk4, label="Susceptible", color="#F4F00B")
-ax1.plot(x, I_rk4, label="Infected", color="#C82909")
-ax1.plot(x, R_rk4, label="Recovered", color="#0BF41B")
-ax1.set_xlabel('Tiempo(s)')
-ax1.set_ylabel('Número de personas')
-ax1.legend()
-ax1.set_title("Evolución de la población")
-
-ax2.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
-ax2.plot(x, N_rk4, color="#27E4F5")
-ax2.set_xlabel('Tiempo(s)')
-ax2.set_ylabel('Número de personas')
-ax2.set_title("Población en el sistema")
-
-plt.savefig("rk4_SIR.pdf")
+plt.savefig("euler_SIR.pdf")
 plt.close()
--- sala/6_EDO/PonceSamuel_S6CASA_EDO_SIR.cpp
+++ sala/6_EDO/PonceSamuel_S6CASA_EDO_SIR.cpp
@@ -6,16 +6,14 @@
 #include <string>
 #include <map>
 
-using map = std::map<std::string, double>;
-using funptr = std::function<double(double, map&, map&)>;
-
 const int N = 200;
 
-double rk4(double h, double people, std::array<double,3> & s, map & p, funptr f, double y_0);
+double f(double people, double I, double S, std::string fun, std::map<std::string, double> & p);
+void rk4(double h, int n, std::array<double, N> & sol, double y_0);
 void doc(std::array<double, N> & data, std::string name);
-std::array<double, 3> f(double people, std::array <double, 3> & s, map & p);
 
 int main(){
+
 
 	//initial conditions
 	double ti = 0.0;
@@ -25,7 +23,7 @@ int main(){
 	double R0 = 0.0;
 	double people = S0+I0+R0;
 
-	map params;//the parameters/constants for modify the system
+	std::map<std::string, double> params;//the parameters/constants for modify the system
 	params["B"] = 3.0;
 	params["r"] = 1.0;
 
@@ -33,78 +31,68 @@ int main(){
 	//elements for euler
 	double N_double = static_cast<double>(N);
 	double h = (tf-ti)/(N_double-1.0);//h for euler
-	std::array<double, N> S_euler;//saving the points for Susceptible
-	std::array<double, N> I_euler;//saving the points for Infected
-	std::array<double, N> R_euler;//saving the point for Recovered
+	std::array<double, N> sol_S;//saving the points for Susceptible
+	std::array<double, N> sol_I;//saving the points for Infected
+	std::array<double, N> sol_R;//saving the point for Recovered
+
 
 	//solving the system with euler
-	S_euler[0] = S0;
-	I_euler[0] = I0;
-	R_euler[0] = R0;
-	map state_e;
+	sol_S[0] = S0;
+	sol_I[0] = I0;
+	sol_R[0] = R0;
 
         for (int i = 1; i<N; ++i){
-		state_e["S"] = S_euler[i-1];
-                state_e["I"] = I_euler[i-1];
-                state_e["R"] = R_euler[i-1];
-		S_euler[i] = S_euler[i-1]+(h*f_S(people, state_e, params));
-		I_euler[i] = I_euler[i-1]+(h*f_I(people, state_e, params));
-		R_euler[i] = R_euler[i-1]+(h*f_R(people, state_e, params));
+		sol_S[i] = sol_S[i-1]+(h*f(people, sol_I[i-1], sol_S[i-1], "S", params));
+		sol_I[i] = sol_I[i-1]+(h*f(people, sol_I[i-1], sol_S[i-1], "I", params));
+		sol_R[i] = sol_R[i-1]+(h*f(people, sol_I[i-1], sol_S[i-1], "R", params));
 	}
 
 	//elements for rk4(Runge-Kutta 4)
-	std::array<double, N> S_rk4;//saving the points for Susceptible
-        std::array<double, N> I_rk4;//saving the points for Infected
-        std::array<double, N> R_rk4;//saving the point for Recovered
-	S_rk4[0] = S0;
-	I_rk4[0] = I0;
-	R_rk4[0] = R0;
+//	std::array<double, N> sol_rk4;
 
 	//calling rk4
-	std::array<double,3> state_rk = [S0, I0, R0];
-        for (int i = 1; i<N; ++i){
-		state_rk["S"] = S_rk4[i-1];
-		state_rk["I"] = I_rk4[i-1];
-		state_rk["R"] = R_rk4[i-1];
-//		std::cout << state_rk["S"];
-                S_rk4[i] = rk4(h, people, state_rk, params, f_S, S_rk4[i-1]);
-                I_rk4[i] = rk4(h, people, state_rk, params, f_I, I_rk4[i-1]);
-                R_rk4[i] = rk4(h, people, state_rk, params, f_R, R_rk4[i-1]);
-        }
-
+//	rk4(h, N, sol_rk4, y_0);
 
 	//making my data
-	doc(S_euler, "myout_Se_SIR.dat");
-	doc(I_euler, "myout_Ie_SIR.dat");
-	doc(R_euler, "myout_Re_SIR.dat");
-        doc(S_rk4, "myout_Srk4_SIR.dat");
-        doc(I_rk4, "myout_Irk4_SIR.dat");
-        doc(R_rk4, "myout_Rrk4_SIR.dat");
+	doc(sol_S, "myout_S_SIR.dat");
+	doc(sol_I, "myout_I_SIR.dat");
+	doc(sol_R, "myout_R_SIR.dat");
 
 	return 0;
-
-std::array<double, 3> f(double people, std::array<double, 3> & s, map & p) {
-
-	double dS = (-p["B"]*s["S"]*s["I"])/people;
-	double dI = ((p["B"]*s["S"]*s["I"])/people)-(p["r"]*s["I"]);
-	double dR = p["r"]*s["I"];
 }
 
-double rk4(double h, double people, std::array<double, 3> & s, map & p, funptr f, double y_0){
+double f(double people, double I, double S, std::string fun, std::map<std::string, double> & p){
 
-	std::array<double, 4> k {};
-	k[0] = h*f(people, s, p);
-	k2 = h*f(people, s, p);
-	s["S"] = S+(k2/2.0);
-	s["I"] = I+(k2/2.0);
-	s["R"] = R+(k2/2.0);
-	k3 = h*f(people, s, p);
-        s["S"] = S+k3;
-        s["I"] = I+k3;
-	s["R"] = R+k3;
-	k4 = h*f(people, s, p);
-	aux = k1 + 2.0*k2 +2.0*k3 + k4;
-	return y_0+((1.0/6.0)*aux);
+	double x{};
+	if(fun=="S"){
+		x = (-p["B"]*S*I)/people;
+	}
+	else if(fun=="I"){
+		x = ((p["B"]*S*I)/people)-(p["r"]*I);
+	}
+	else if(fun=="R"){
+		x = p["r"]*I;
+	}
+
+	return x;
+}
+
+void rk4(double h, int n, std::array<double, N> & sol, double y_0){
+	sol[0] = y_0;
+	double k1 = 0.0;
+	double k2 = 0.0;
+	double k3 = 0.0;
+	double k4 = 0.0;
+	double aux = 0.0;
+
+        for (int i = 1; i<n; ++i){
+//		k1 = h*f(sol[i-1]);
+//		k2 = h*f(sol[i-1]+(k1/2.0));
+//		k3 = h*f(sol[i-1]+(k2/2.0));
+//		k4 = h*f(sol[i-1]+k3);
+//		aux = k1 + 2.0*k2 +2.0*k3 + k4;
+//		sol[i] = sol[i-1]+((1.0/6.0)*aux);
+        }
 }
 
 
