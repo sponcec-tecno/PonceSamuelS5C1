f46c78557013d7872cbac11174ba7de853b93571
sponcec-tecno <sponcec@unal.edu.co>
2026-05-10 00:50:39 -0500

    El make ya funciona con los nuevos nombres

--- sala/maker.mk
+++ sala/maker.mk
@@ -1,14 +1,17 @@
 .PHONY : data
-data : myout_euler.dat myout_rk4.dat
-%.dat : PonceSamuel_S6C1_EDO.cpp
+data : myout_euler.dat myout_rk4.dat myout_I.dat myout_R.dat myout_S.dat
+%.dat : PonceSamuel_S6C1_EDO.cpp PonceSamuel_S6CASA_EDO_SIR.cpp
 	g++ $<
+	./a.out
+	g++ PonceSamuel_S6CASA_EDO_SIR.cpp
 	./a.out
 
 .PHONY : plots
-plots : all.pdf euler.pdf rk4.pdf
+plots : all.pdf euler.pdf rk4.pdf SIR.pdf
 
-%.pdf: PLOTS_PonceSamuel_S6CASA_EDO.py myout_euler.dat myout_rk4.dat
+%.pdf: PLOTS_PonceSamuel_S6CASA_EDO.py PLOTS_PonceSamuel_S6CASA_EDO_SIR.py myout_euler.dat myout_rk4.dat myout_S.dat myout_I.dat myout_R.dat
 	python $<
+	python PLOTS_PonceSamuel_S6CASA_EDO_SIR.py
 
 .PHONY : clean
 clean :
