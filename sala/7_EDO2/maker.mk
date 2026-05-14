7491f828d9f03d17fe8e6afe7dce3b890af64027
sponcec-tecno <sponcec@unal.edu.co>
2026-05-13 19:30:33 -0500

    Agregado para froggy, funciona

--- sala/7_EDO2/maker.mk
+++ sala/7_EDO2/maker.mk
@@ -1,12 +1,12 @@
 .PHONY : data
-data : myout_x_e.dat myout_v_e.dat
+data : myout_x_e.dat myout_v_e.dat myout_x_lf.dat myout_v_lf.dat
 %.dat : PonceSamuel_S7C1_EDO2ordenRESORTE.cpp
-	g++ $< -o a1.out
-	./a1.out
+	g++ $< -o a.out
+	./a.out
 
 .PHONY : plot
 plot : v.pdf x.pdf
-%.pdf: PLOTS_PonceSamuel_S7C1_EDO2.py myout_x_e.dat myout_v_e.dat
+%.pdf: PLOTS_PonceSamuel_S7C1_EDO2.py myout_x_e.dat myout_v_e.dat myout_x_lf.dat myout_v_lf.dat
 	python $<
 
 .PHONY : clean
@@ -14,3 +14,5 @@ clean :
 	rm -f *.dat
 	rm -f *.pdf
 	rm -f *.out
+
+#¿Se puede ingresar el argumento para el commit de git?
