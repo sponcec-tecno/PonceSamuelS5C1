c249dfb89a34983e4b5209611be190c4522abb2b
sponcec-tecno <sponcec@unal.edu.co>
2026-05-13 19:30:00 -0500

    Se agregó el plot para el analítico, como función coseno

--- sala/7_EDO2/PLOTS_PonceSamuel_S7C1_EDO2.py
+++ sala/7_EDO2/PLOTS_PonceSamuel_S7C1_EDO2.py
@@ -4,6 +4,14 @@ import matplotlib.pylab as plt
 #Dinámica de la partícula
 #Tiempos
 t = np.linspace(0.0, 1.0, 200)
+#Teórico
+A = 0.1
+k = 50
+m = 0.2
+w = np.sqrt(k/m)
+
+x_teo = A*np.cos(w*t)
+v_teo = -w*A*np.sin(w*t)
 #Recibo los datos
 x_e = np.genfromtxt("myout_x_e.dat")
 v_e = np.genfromtxt("myout_v_e.dat")
@@ -17,6 +25,7 @@ fig, ax = plt.subplots()
 
 ax.plot(t, v_e, color="#F4A20B", label="Euler")
 ax.plot(t, v_lf, color="#5DF40B", label="Leap-Frog")
+ax.plot(t, v_teo, label="Teórico")#Comparé con el teórico, eso sí, teniendo en cuenta que están desfasados
 ax.set_xlabel('Tiempo(s)')
 ax.set_ylabel('Velocidad(m/s)')
 ax.set_title("Velocidad en función del tiempo")
@@ -30,7 +39,8 @@ plt.close()
 fig, ax = plt.subplots()
 
 ax.plot(t, x_e, color="#F4A20B", label="Euler")
-ax.plot(t, v_lf, color="#5DF40B", label="Leap-Frog")
+ax.plot(t, x_lf, color="#5DF40B", label="Leap-Frog")
+ax.plot(t, x_teo, label="Teórico")#Comparé con el teórico, eso sí, teniendo en cuenta que están desfasados
 ax.set_xlabel('Tiempo(s)')
 ax.set_ylabel('Posición(m)')
 ax.set_title("Posición en función del tiempo")
