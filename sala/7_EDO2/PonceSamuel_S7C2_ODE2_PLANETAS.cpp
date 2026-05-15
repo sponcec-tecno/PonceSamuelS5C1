0af70625276fac6e550809b05c50e9b6dc21df80
sponcec-tecno <sponcec@unal.edu.co>
2026-05-14 19:13:10 -0500

    No funciona, ya revisé la lógica del código y nada

--- sala/7_EDO2/PonceSamuel_S7C2_ODE2_PLANETAS.cpp
+++ sala/7_EDO2/PonceSamuel_S7C2_ODE2_PLANETAS.cpp
@@ -11,8 +11,7 @@
 #include <string>
 #include <map>
 
-const int N = 1000;
-
+const int N = 200;
 void doc(std::array<double, N> & data, std::string name);
 
 int main(){
@@ -20,25 +19,28 @@ int main(){
 
 	//initial conditions
 	double ti = 0.0;
-	double tf = 365.0;
-	double x0 = 1.000003;
+	double tf = 1.0;//un año
+	double x0 = 1.000003;//UA
 	double vx0 = 0.0;
 	double y0 = 0.0;
-	double vy0 = 0.1;
+	double vy0 = 0.006;//UA/año
+	double xS = 0.01671;//Distancia del Sol al centro del sistema solar
+	double yS = 0.0;
+
 
 	std::map<std::string, double> p;//the parameters/constants for modify the system
-	p["G"] = 0.0002959;
+	p["G"] = 39.436;//Constante en años, UA y MA.
 	p["M"] = 1.0;
-
+	std::cout << p["G"] << "\t" << p["M"] << "\n";
 
 	//Euler
 	//elements for euler
 	double N_double = static_cast<double>(N);
 	double h = (tf-ti)/(N_double-1.0);//h for euler
-	std::array<double, N> x_e;//saving the points for position
-	std::array<double, N> vx_e;//saving the points for velocity
-	std::array<double, N> y_e;//saving the points for position
-        std::array<double, N> vy_e;//saving the points for velocity
+	std::array<double, N> x_e;//saving the points for xposition
+	std::array<double, N> vx_e;//saving the points for xvelocity
+	std::array<double, N> y_e;//saving the points for yposition
+        std::array<double, N> vy_e;//saving the points for yvelocity
 
 
 	//solving the system with euler
@@ -49,12 +51,16 @@ int main(){
 	double r = 0.0;
 
 	for (int i = 1; i<N; ++i){
-		r = std::sqrt((x_e[i-1]*x_e[i-1])+(y_e[i-1]*y_e[i-1]));
-		std::cout << r << "\t";
+		r = std::pow(x_e[i-1]-xS,2.0)+std::pow(y_e[i-1]-yS,2.0);
+		std::cout << i-1 << "\t" << r << "\n";
+//		x_e[i] = x_e[i-1]+(h*vx_e[i-1]);
+//		vx_e[i] = vx_e[i-1]+(h*((-p["G"]*p["M"])/(std::pow(r,3.0)))*x_e[i-1]);
+//		y_e[i] = y_e[i-1]+(h*vy_e[i-1]);
+//		vy_e[i] = vy_e[i-1]+(h*((-p["G"]*p["M"])/(std::pow(r,3.0)))*y_e[i-1]);
 		x_e[i] = x_e[i-1]+(h*vx_e[i-1]);
-		vx_e[i] = vx_e[i-1]+(h*(-p["G"]*p["M"]/(std::pow(r,3)))*x_e[i-1]);
+		vx_e[i] = vx_e[i-1]+(h*(-(x_e[i-1]*p["G"]*p["M"]/(r*std::sqrt(r)))));
                 y_e[i] = y_e[i-1]+(h*vy_e[i-1]);
-                vy_e[i] = vy_e[i-1]+(h*(-p["G"]*p["M"]/(std::pow(r,3)))*y_e[i-1]);
+                vy_e[i] = vy_e[i-1]+(h*(-(y_e[i-1]*p["G"]*p["M"]/(r*std::sqrt(r)))));
 	}
 
 
