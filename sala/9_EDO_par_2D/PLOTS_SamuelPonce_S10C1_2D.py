06b66ebf31855324289f127e116a4c489d35981e
sponcec-tecno <sponcec@unal.edu.co>
2026-06-04 09:29:33 -0500

    Se ve mejor, y colorbars para cada uno

--- sala/9_EDO_par_2D/PLOTS_SamuelPonce_S10C1_2D.py
+++ sala/9_EDO_par_2D/PLOTS_SamuelPonce_S10C1_2D.py
@@ -18,24 +18,37 @@ ax2 = fig.add_subplot(gs1[1])
 ax3 = fig.add_subplot(gs1[2])
 ax4 = fig.add_subplot(gs1[3])
 
-ax1.matshow(T0)
+#Donde varian mis datos
+#norm = col.Normalize(vmin=50.0, vmax=100.0)
+
+#Mi paleta de colores
+color = "rainbow"
+
+#Ploteo las placas
+im1 = ax1.matshow(T0, cmap=color)
+plt.colorbar(im1)
 ax1.set_title("0 seg")
-ax2.matshow(T1)
+
+im2 = ax2.matshow(T1, cmap=color)
+plt.colorbar(im2)
 ax2.set_title("100 seg")
-ax3.matshow(T2)
+
+im3 = ax3.matshow(T2, cmap=color)
+plt.colorbar(im3)
 ax3.set_title("1000 seg")
-ax4.matshow(T3)
+
+im4 = ax4.matshow(T3, cmap=color)
+plt.colorbar(im4)
 ax4.set_title("2500 seg")
 
 #Barra de color
-cax = plt.axes([0.85, 0.1, 0.075, 0.8])
-norm = col.Normalize(vmin=50.0, vmax=100.0)
-map = cm.ScalarMappable(norm=norm, cmap="summer")
+#cax = plt.axes([0.85, 0.1, 0.075, 0.8])
+#map = cm.ScalarMappable(norm=norm, cmap=color)
 
-plt.colorbar(mappable=map, cax=cax)
+#plt.colorbar(mappable=map, cax=cax)
 
 #Organizo
-gs1.tight_layout(fig, rect=[0, 0, 0.9, 0.95])
+gs1.tight_layout(fig, rect=[0, 0, 1.0, 0.95])
 
 fig.suptitle("Placa en cuatro momentos")
 plt.savefig("sol_difussion.pdf")
